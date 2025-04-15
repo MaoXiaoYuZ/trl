b19cb5e1dd990a5d5366121c1ad9d9f8080e08de
MaoXiaoYuZ <1275465626@qq.com>
2025-04-15 09:45:25 +0800

    修改A800上运行的BatchSize

--- run_ppo_tldr.sh
+++ run_ppo_tldr.sh
@@ -1,11 +1,11 @@
-# PYTHONPATH=/hy-tmp/trl:$PYTHONPATH python examples/scripts/ppo/ppo_tldr.py \
-PYDEVD_WARN_SLOW_RESOLVE_TIMEOUT=5s PYTHONPATH=/hy-tmp/trl:$PYTHONPATH python -m debugpy --wait-for-client --listen 5678  examples/scripts/ppo/ppo_tldr.py \
+# PYDEVD_WARN_SLOW_RESOLVE_TIMEOUT=5s PYTHONPATH=/hy-tmp/trl:$PYTHONPATH python -m debugpy --wait-for-client --listen 5678  examples/scripts/ppo/ppo_tldr.py \
+PYTHONPATH=/hy-tmp/trl:$PYTHONPATH python examples/scripts/ppo/ppo_tldr.py \
     --dataset_name trl-internal-testing/tldr-preference-sft-trl-style \
     --dataset_test_split validation \
     --learning_rate 3e-6 \
     --output_dir models/minimal/ppo_tldr \
-    --per_device_train_batch_size 1 \
-    --gradient_accumulation_steps 64 \
+    --per_device_train_batch_size 4 \
+    --gradient_accumulation_steps 16 \
     --total_episodes 30000 \
     --model_name_or_path EleutherAI/pythia-1b-deduped \
     --sft_model_path cleanrl/EleutherAI_pythia-1b-deduped__sft__tldr \
